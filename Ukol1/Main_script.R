d1f913c9fbeb0e14e1e4cc6d8310fa7e419d8300
Petr Chaloupek <petrchaloupek@gmail.com>
2025-03-12 19:45:01 +0100

    work_hard:D

--- Ukol1/Main_script.R
+++ Ukol1/Main_script.R
@@ -1,5 +1,5 @@
 ################################################################################
-######################## CASOVE RADY - UKOL Č. 1 ###############################
+######################## CASOVE RADY - UKOL C. 1 ###############################
 ################################################################################
 
 rm(list = ls())
@@ -29,7 +29,9 @@ IR <- fredr(
   observation_end = as.Date("2025-02-01")
 )
 
-############################### ULOHA Č. 1 #####################################
+############################### ULOHA C. 1 #####################################
+# Vykresleni casovych rad
+
 ggplot()+
   geom_line(data = CPI, aes(x = CPI$date, y = CPI$value), color ="#4BACC6") +
   theme_bw() +
@@ -44,4 +46,37 @@ ggplot()+
   labs(x = "Datum", y = "IR") +
   scale_x_date(date_labels = "%Y", date_breaks = "10 years")
 
-  
+# Uprava dat
+
+CPI$value_stac <- c(NA,diff(log(CPI$value)))
+
+IR$value_stac <- c(NA,diff(IR$value))
+
+# Vykresleni novych casovych rad
+
+ggplot()+
+  geom_line(data = CPI, aes(x = CPI$date, y = CPI$value_stac), color ="#4BACC6") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+  labs(x = "Datum", y = "Delta log CPI") +
+  scale_x_date(date_labels = "%Y", date_breaks = "10 years")
+
+ggplot()+
+  geom_line(data = IR, aes(x = IR$date, y = IR$value_stac), color = "#17a589") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+  labs(x = "Datum", y = "Delta IR") +
+  scale_x_date(date_labels = "%Y", date_breaks = "10 years")
+
+############################### ULOHA C. 2 #####################################
+
+# ACD a PACF casovych rad
+
+par(mfrow = c(1,2))
+acf(CPI$value_stac, lag.max = 20, na.action = na.pass, col = "#4BACC6")
+pacf(CPI$value_stac, lag.max = 20, na.action = na.pass, col = "#4BACC6")
+
+par(mfrow = c(1,2))
+acf(IR$value_stac, lag.max = 20, na.action = na.pass, col = "#17a589")
+pacf(IR$value_stac, lag.max = 20, na.action = na.pass, col = "#17a589")
+
